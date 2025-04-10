9fdd0c5bba2227eb53872c15eccdcc991fd1ff8a
purbatua <purbatua.pasaribu@gmail.com>
2025-04-10 17:35:20 +0700

    chore: update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,31 +19,31 @@ const config: NextConfig = {
     },
   },
   allowedDevOrigins: (process.env.CORS_ALLOW_LIST || "").split(","),
-  headers: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Access-Control-Allow-Origin",
-            value: process.env.CORS_ALLOW_LIST ? `http://${process.env.CORS_ALLOW_LIST}` : "", // Set your origin
-          },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET, POST, PUT, DELETE, OPTIONS",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version", // "Content-Type, Authorization",
-          },
-          {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          }
-        ],
-      },
-    ];
-  },
+  // headers: async () => {
+  //   return [
+  //     {
+  //       source: "/api/:path*",
+  //       headers: [
+  //         {
+  //           key: "Access-Control-Allow-Origin",
+  //           value: process.env.CORS_ALLOW_LIST ? `http://${process.env.CORS_ALLOW_LIST}` : "", // Set your origin
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Methods",
+  //           value: "GET, POST, PUT, DELETE, OPTIONS",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Headers",
+  //           value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version", // "Content-Type, Authorization",
+  //         },
+  //         {
+  //           key: "Access-Control-Allow-Credentials",
+  //           value: "true",
+  //         }
+  //       ],
+  //     },
+  //   ];
+  // },
   rewrites: async () => [
     { source: "/healthz", destination: "/api/health" },
     { source: "/api/healthz", destination: "/api/health" },
